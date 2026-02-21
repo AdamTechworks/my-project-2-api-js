21a98cd25529b25b270a1d8d78ca53d42052487e
Adam Ellison <adamtechworks@gmail.com>
2026-02-21 15:14:06 -0500

    Added About page and fixed nav links

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,11 +1,12 @@
-export function loadNav(activePage) {
-  const nav = document.createElement("nav");
+// nav for all pages
+export function loadNav(activePage = "") {
+  const nav = document.getElementById("nav");
+  if (!nav) return;
 
   nav.innerHTML = `
-    <a href="/index.html" ${activePage === "home" ? 'style="font-weight:bold;"' : ""}>Home</a> |
-    <a href="/catalog.html" ${activePage === "catalog" ? 'style="font-weight:bold;"' : ""}>Catalog</a>
+    <a href="/index.html" class="${activePage === "home" ? "active" : ""}">Home</a> |
+    <a href="/catalog.html" class="${activePage === "catalog" ? "active" : ""}">Catalog</a> |
+    <a href="/about.html" class="${activePage === "about" ? "active" : ""}">About</a>
     <hr />
   `;
-
-  document.body.insertBefore(nav, document.body.firstChild);
 }
