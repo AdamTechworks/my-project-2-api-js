86d93a8d6a7da3478688344d5a74fc342ccf63bb
Adam Ellison <adamtechworks@gmail.com>
2026-05-11 22:31:55 -0400

    corrected folder and file paths to communicate better UX

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+// nav for all pages
+export function loadNav(activePage = "") {
+  const nav = document.getElementById("nav");
+  if (!nav) return;
+
+  nav.innerHTML = `
+    <a href="./index.html" class="${activePage === "home" ? "active" : ""}">Home</a> |
+    <a href="./catalog.html" class="${activePage === "catalog" ? "active" : ""}">Catalog</a> |
+    <a href="./about.html" class="${activePage === "about" ? "active" : ""}">About</a> |
+    <a href="./contact.html" class="${activePage === "contact" ? "active" : ""}">Contact</a>
+    <hr />
+  `;
+}
